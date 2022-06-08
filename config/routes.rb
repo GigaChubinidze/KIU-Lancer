0a75543f55576a9415658c7d6e4fadb61d8f1b23
kiu_chubinidze.giga <chubinidze.giga@kiu.edu.ge>
2022-06-08 23:51:45 +0400

    user crud routes and navbar done

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: "registrations"
+  }
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  root "store#index"
+end
